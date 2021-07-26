04896727df260685c5667716555baa09fe648eef
ZavierHenry <bigz.henry63@gmail.com>
2021-07-25 22:04:55 -0500

    Simplify build example filepath script

--- scripts/buildExampleFilepath.sh
+++ scripts/buildExampleFilepath.sh
@@ -2,4 +2,4 @@
 FILENAME='exampleFilepaths.txt'
 TWEET_DIRECTORY="tweets/"
 
-find "./$TWEET_DIRECTORY" -type f -name '*.json' | sed "s/\.\/$(echo $TWEET_DIRECTORY | sed 's/\//\\\//g')//" > FILENAME
+find "$TWEET_DIRECTORY" -type f -name '*.json' | sed "s#$TWEET_DIRECTORY##"
